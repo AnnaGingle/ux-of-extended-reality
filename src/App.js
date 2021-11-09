bca4f79b593b10de08be0f9b566b4e95a836ef20
Anna Gingle <326488@colum.edu>
2021-11-09 10:19:37 -0600

    header styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* App-header-container used for bg styles */}
+      <div className="App-header-container" style={{background: "linear-gradient(#e66465, #9198e5);" }} >
+
+        <header className="App-header">
+          <div className="Logo"><p>UX of XR</p></div>
+          <div className="Header-one">
+            <p>
+              Extended reality shapes our perception and therefore how we see the world 
+            </p>
+          </div>
+        </header>
+      </div>
+     
+   
+      <div className="Main">
+
+      </div>
+      {/*App div*/}
     </div>
   );
 }
